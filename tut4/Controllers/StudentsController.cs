3e0ccbbc063ad53e8db07ffa25207b44324c2c59
Andrii Vlasenko <s19144@pjwstk.edu.pl>
2020-03-31 23:07:12 +0200

    commit

---
+++ tut4/Controllers/StudentsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tut4.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private string ConStr = "Data Source=db-mssql.pjwstk.edu.pl;Initial Catalog=2019SBD;Integrated Security=True";
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+
+            using (SqlConnection con = new SqlConnection(ConStr))
+            using (SqlCommand com=new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "Select * from Student";
+                
+
+                con.Open();     
+                
+
+            }
+
+            return Ok();
+        }
+    }
+}
